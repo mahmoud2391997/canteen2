571db2fcfbf9d6c8b6cf01b8ec34ccf29756d884
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2023-03-01 17:19:57 +0200

    add to cart

--- lib/variables.dart
+++ lib/variables.dart
@@ -16,9 +16,8 @@ class product extends StatefulWidget {
   final int price;
   final String image;
   int boolean;
-  bool boolian;
+  bool? boolian;
   String doc;
-  static List<dynamic>? global;
   int quantity;
 
   product({
@@ -27,7 +26,7 @@ class product extends StatefulWidget {
     required this.price,
     required this.image,
     required this.boolean,
-    required this.boolian,
+    this.boolian,
     required this.doc,
     required this.quantity,
   });
@@ -92,109 +91,157 @@ class _productState extends State<product> {
             ),
             MaterialButton(
               onPressed: () async {
-                if (widget.boolian == false) {
-                  int qSnap = (await order.count().get()).count + 1;
-                  CollectionReference cartItem =
-                      FirebaseFirestore.instance.collection('cart');
-                  await cartItem.doc(P.toString()).set({
-                    'cart item': '${widget.name}',
-                    'price': '${widget.price}',
-                    'image': '${widget.image}',
-                    'quantity': '${widget.quantity}',
+                FirebaseFirestore.instance
+                    .collection('cart')
+                    .get()
+                    .then((value) async {
+                  value.docs.forEach((element) async {
+                    if (widget.name == element.data()['cart item']) {
+                      widget.boolian = true;
+                    }
                   });
+                  if (widget.boolian == false ||
+                      value.docs.isEmpty ||
+                      widget.boolian == null) {
+                    int qSnap = (await order.count().get()).count + 1;
+                    CollectionReference cartItem =
+                        FirebaseFirestore.instance.collection('cart');
+                    await cartItem.doc().set({
+                      'cart item': '${widget.name}',
+                      'price': '${widget.price}',
+                      'image': '${widget.image}',
+                      'quantity': '${widget.quantity}',
+                    });
 
-                  // var documentReference = FirebaseFirestore.instance
-                  //     .collection('orders')
-                  //     .doc('order')
-                  //     .collection('order')
-                  //     .doc('order');
-                  // FirebaseFirestore.instance
-                  //     .runTransaction((transaction) async {
-                  //   transaction.set(
-                  //     documentReference,
-                  //     {
-                  //       'order item': '${widget.name}',
-                  //       'price': '${widget.price}',
-                  //       'image': '${widget.image}',
-                  //       'quantity': '${widget.quantity}'
-                  //     },
-                  //   );
-                  // });
-
-                  QuerySnapshot<Map<String, dynamic>> _query =
-                      await FirebaseFirestore.instance
+                    QuerySnapshot<Map<String, dynamic>> _query =
+                        await FirebaseFirestore.instance
+                            .collection('orders')
+                            .get();
+                    QuerySnapshot<Map<String, dynamic>> _queryy =
+                        await FirebaseFirestore.instance
+                            .collectionGroup('order')
+                            .get();
+                    if (_query.docs.isNotEmpty && _queryy.docs.isNotEmpty) {
+                      final QuerySnapshot Q = await FirebaseFirestore.instance
                           .collection('orders')
                           .get();
-                  QuerySnapshot<Map<String, dynamic>> _queryy =
-                      await FirebaseFirestore.instance
-                          .collectionGroup('order')
+                      f = Q.docs.length + 1;
+                    } else if (_query.docs.isEmpty) {
+                      f = 1;
+                    } else {
+                      final QuerySnapshot Q = await FirebaseFirestore.instance
+                          .collection('orders')
                           .get();
-                  if (_query.docs.isNotEmpty && _queryy.docs.isNotEmpty) {
-                    final QuerySnapshot Q = await FirebaseFirestore.instance
-                        .collection('orders')
-                        .get();
-                    f = Q.docs.length + 1;
-                  } else if (_query.docs.isEmpty) {
-                    f = 1;
+                      f = Q.docs.length;
+                    }
+
+                    ;
+                    CollectionReference u =
+                        FirebaseFirestore.instance.collection('orders');
+
+                    if (boolean == true) {
+                      var r = await u.add({'order number': '${f}'});
+                      u.doc(r.id).collection('order').add({
+                        'order item': '${widget.name}',
+                        'price': '${widget.price}',
+                        'image': '${widget.image}',
+                        'quantity': '${widget.quantity}'
+                      });
+                      l = r.id;
+                      boolean = false;
+                    } else {
+                      u.doc(l).collection('order').add({
+                        'order item': '${widget.name}',
+                        'price': '${widget.price}',
+                        'image': '${widget.image}',
+                        'quantity': '${widget.quantity}'
+                      });
+                    }
+
+                    if (mounted) {
+                      setState(() {
+                        P++;
+
+                        I++;
+                        // widget.boolian = false;
+                      });
+                    }
+                    showToast(text: 'Added to cart', color: Colors.amberAccent);
                   } else {
-                    final QuerySnapshot Q = await FirebaseFirestore.instance
-                        .collection('orders')
-                        .get();
-                    f = Q.docs.length;
+                    showToast(
+                        text: 'Already in cart!', color: Colors.amberAccent);
                   }
+                });
 
-                  ;
-                  CollectionReference u =
-                      FirebaseFirestore.instance.collection('orders');
+                // if (widget.boolian == false) {
+                //   int qSnap = (await order.count().get()).count + 1;
+                //   CollectionReference cartItem =
+                //       FirebaseFirestore.instance.collection('cart');
+                //   await cartItem.doc().set({
+                //     'cart item': '${widget.name}',
+                //     'price': '${widget.price}',
+                //     'image': '${widget.image}',
+                //     'quantity': '${widget.quantity}',
+                //   });
 
-                  if (boolean == true) {
-                    var r = await u.add({'order number': '${f}'});
-                    u.doc(r.id).collection('order').add({
-                      'order item': '${widget.name}',
-                      'price': '${widget.price}',
-                      'image': '${widget.image}',
-                      'quantity': '${widget.quantity}'
-                    });
-                    l = r.id;
-                    boolean = false;
-                  } else {
-                    u.doc(l).collection('order').add({
-                      'order item': '${widget.name}',
-                      'price': '${widget.price}',
-                      'image': '${widget.image}',
-                      'quantity': '${widget.quantity}'
-                    });
-                  }
+                //   QuerySnapshot<Map<String, dynamic>> _query =
+                //       await FirebaseFirestore.instance
+                //           .collection('orders')
+                //           .get();
+                //   QuerySnapshot<Map<String, dynamic>> _queryy =
+                //       await FirebaseFirestore.instance
+                //           .collectionGroup('order')
+                //           .get();
+                //   if (_query.docs.isNotEmpty && _queryy.docs.isNotEmpty) {
+                //     final QuerySnapshot Q = await FirebaseFirestore.instance
+                //         .collection('orders')
+                //         .get();
+                //     f = Q.docs.length + 1;
+                //   } else if (_query.docs.isEmpty) {
+                //     f = 1;
+                //   } else {
+                //     final QuerySnapshot Q = await FirebaseFirestore.instance
+                //         .collection('orders')
+                //         .get();
+                //     f = Q.docs.length;
+                //   }
 
-                  // CollectionReference orderedItem = FirebaseFirestore.instance
-                  //     .collection('orders')
-                  //     .doc()
-                  //     .collection('order');
-                  // await orderedItem.doc().set({
-                  //   'order item': '${widget.name}',
-                  //   'price': '${widget.price}',
-                  //   'image': '${widget.image}',
-                  //   'quantity': '${widget.quantity}',
-                  // });
-                  // CollectionReference orders =
-                  //     FirebaseFirestore.instance.collection('orders');
+                //   ;
+                //   CollectionReference u =
+                //       FirebaseFirestore.instance.collection('orders');
 
-                  // QuerySnapshot result = await orders.get();
-                  // DocumentSnapshot res = result.docs[order].get('order number');
-                  // await orders.doc('$order').set({'order number': '$order'});
-                  if (mounted) {
-                    setState(() {
-                      P++;
+                //   if (boolean == true) {
+                //     var r = await u.add({'order number': '${f}'});
+                //     u.doc(r.id).collection('order').add({
+                //       'order item': '${widget.name}',
+                //       'price': '${widget.price}',
+                //       'image': '${widget.image}',
+                //       'quantity': '${widget.quantity}'
+                //     });
+                //     l = r.id;
+                //     boolean = false;
+                //   } else {
+                //     u.doc(l).collection('order').add({
+                //       'order item': '${widget.name}',
+                //       'price': '${widget.price}',
+                //       'image': '${widget.image}',
+                //       'quantity': '${widget.quantity}'
+                //     });
+                //   }
 
-                      I++;
-                      widget.boolian = true;
-                    });
-                  }
-                  showToast(text: 'Added to cart', color: Colors.amberAccent);
-                } else {
-                  showToast(
-                      text: 'Already in cart!', color: Colors.amberAccent);
-                }
+                //   if (mounted) {
+                //     setState(() {
+                //       P++;
+
+                //       I++;
+                //       widget.boolian = true;
+                //     });
+                //   }
+                //   showToast(text: 'Added to cart', color: Colors.amberAccent);
+                // } else {
+                //   showToast(
+                //       text: 'Already in cart!', color: Colors.amberAccent);
+                // }
               },
               color: Color.fromARGB(255, 5, 3, 85),
               child: Text(
